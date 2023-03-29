df135b78b8bc016997deddc5c227babceffa987c
netrivas <dcobos00@gmail.com>
2023-03-29 18:27:28 +0000

    third answer

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const fiveToOneHundred = () => {
         console.log(i)
     }
 }
-fiveToOneHundred()
+//fiveToOneHundred()
 
 const multiplesOfThree = () => {
     for (let i=3; i<=100; i=i+3){
- console.log(i)
-
+        console.log(i);
     }
 }
+//multiplesOfThree()
 
-multiplesOfThree()
+const multiplesOfThreeOrFive = () => {
+   for (let i=1; i<=100; i++){
+       if (i % 3 === 0 || i % 5 === 0){
+           console.log(i)
+       }
+   }
+}
+//multiplesOfThreeOrFive()
